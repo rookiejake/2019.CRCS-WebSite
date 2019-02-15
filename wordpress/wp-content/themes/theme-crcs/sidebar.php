91480dd852fdd6e9f9aec8e7fe38099463faecbe
Joe V <joevillegasisawesome@gmail.com>
2019-02-14 19:17:32 -0800

    Fixed refactoring of redirects within website

--- wordpress/wp-content/themes/theme-crcs/sidebar.php
+++ wordpress/wp-content/themes/theme-crcs/sidebar.php
@@ -3,12 +3,12 @@
     <div id="navbar" class="stick">
         <nav class="main-navigation">
             <ul>
-    <li><a href="/csscts/CRCS/">Home</a></li>
-    <li><a href="/csscts/CRCS/Projects">Projects</a></li>
-    <li><a href="/csscts/CRCS/ImagineCup">Imagine Cup</a></li>
-    <li><a href="/csscts/CRCS/Publications">Publications</a></li>
-    <li><a href="/csscts/CRCS/Documentation">Documentation</a></li>
-                    <li><a href="/csscts/CRCS/Dissemination">Dissemination</a></li>
+    <li><a href="/2019.CRCS-WebSite/">Home</a></li>
+    <li><a href="/2019.CRCS-WebSite/Projects">Projects</a></li>
+    <li><a href="/2019.CRCS-WebSite/ImagineCup">Imagine Cup</a></li>
+    <li><a href="/2019.CRCS-WebSite/Publications">Publications</a></li>
+    <li><a href="/2019.CRCS-WebSite/Documentation">Documentation</a></li>
+                    <li><a href="/2019.CRCS-WebSite/Dissemination">Dissemination</a></li>
             </ul>
         </nav>
     </div>
